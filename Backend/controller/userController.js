5ddc748eae6fd2147514e696c887cef2e51951fe
Potrirajan A <potrirajan16@gmail.com>
2026-07-10 16:29:02 +0530

    frontend create

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,50 +1,94 @@
 import Users from "../models/user.js";
 
 // ALL USERS
-export const getAllUser = async (req,res) =>{
-    const {page=1, limit=5, search =""} = req.query;
+export const getAllUser = async (req, res) => {
+  try {
+    const { page = 1, limit = 5, search = "" } = req.query;
     const query = {
-        name : {$regex: search, $options: "i"},
-    }; 
+      name: { $regex: search, $options: "i" },
+    };
 
     const total = await Users.countDocuments(query);
     const users = await Users.find(query)
-    .skip((page-1)*limit)
-    .limit(Number(limit));
-    res.json({
-        data:users,
-        total,
-        totalPages : Math.ceil(total/limit),
-    });  
+      .skip((page - 1) * limit)
+      .limit(Number(limit));
+    res.status(201).json({
+      success: true,
+      data: users,
+      total,
+      totalPages: Math.ceil(total / limit),
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: `error:${err}`,
+    });
+  }
 };
 
 // SINGLE USERS
-export const getSingleUser = async (req,res) =>{
+export const getSingleUser = async (req, res) => {
+  try {
     const id = req.params.id;
     const user = await Users.findById(id);
-    res.json({
-        data:user
+    res.status(200).json({
+      success: true,
+      data: user,
     });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: `error:${err}`,
+    });
+  }
 };
 
 // ADD USERS
-export const addUsers = async (req,res)=>{
+export const addUsers = async (req, res) => {
+  try {
     const user = await Users.create(req.body);
-    res.json(user);
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: `error:${err}`,
+    });
+  }
 };
 
 // UPDATE USERS
-export const updateUser = async(req,res)=>{
+export const updateUser = async (req, res) => {
+  try {
     const id = req.params.id;
-    const user = await Users.findByIdAndUpdate(id, req.body, {new:true});
-    res.json({
-        data:user
+    const user = await Users.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(201).json({
+      success: true,
+      data: user,
     });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: `error:${err}`,
+    });
+  }
 };
 
 // DELETE USERS
-export const deleteUser = async(req,res)=>{
+export const deleteUser = async (req, res) => {
+  try {
     const id = req.params.id;
     await Users.findByIdAndDelete(id);
-    res.json({message:"Data deleted Success..."});
+    res.json({
+      success: true,
+      message: "Data deleted Success...",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: `error:${err}`,
+    });
+  }
 };
